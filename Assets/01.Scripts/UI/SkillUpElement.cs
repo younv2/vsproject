3dc52c84b45edca700f864e021556abb5a286c45
DESKTOP-9NG2MRK\younv <younv2@naver.com>
2025-03-18 23:46:10 +0900

    GUI asset 추가

--- Assets/01.Scripts/UI/SkillUpElement.cs
+++ Assets/01.Scripts/UI/SkillUpElement.cs
@@ -5,11 +5,15 @@ using UnityEngine.UI;
 public class SkillUpElement : MonoBehaviour
 {
     public TextMeshProUGUI nameText;
+    public TextMeshProUGUI lvText;
+    public TextMeshProUGUI descriptionText;
     private SkillDataBase skillData;
     public void Setting(SkillDataBase data,SkillUpPopup popup)
     {
         skillData = data;
         nameText.text = skillData.name;
+        lvText.text = skillData.name;
+        descriptionText.text = skillData.name;
         Button button = this.GetComponent<Button>();
         button.onClick.RemoveAllListeners();
         button.onClick.AddListener(() =>
